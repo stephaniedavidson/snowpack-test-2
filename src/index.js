8f671febff19c97c2caeaefeb0a6e3ede87f4e7c
Steph Davidson <stephcdavidson@gmail.com>
2021-03-02 11:57:34 -0500

    working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { helloWorld } from './helloworld.js';
 import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 
-
 confetti.create(document.getElementById('canvas'), {
     resize: true,
     useWorker: true,
