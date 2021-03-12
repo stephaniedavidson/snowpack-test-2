43f19c8edc5cc3815ce5e953684c540944ce3ee3
Steph Davidson <stephcdavidson@gmail.com>
2021-03-12 10:37:43 -0500

    working but kind of sucks

---
+++ public/index.js
@@ -0,0 +1,9 @@
+import { helloWorld } from './helloworld.js';
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+
+confetti.create(document.getElementById('canvas'), {
+    resize: true,
+    useWorker: true,
+   })({ particleCount: 200, spread: 200 });
+
+helloWorld();
