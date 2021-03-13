b703aab2c5a6eb104f809e0953e8b0c68197e29f
Steph Davidson <stephcdavidson@gmail.com>
2021-03-13 15:12:40 -0500

    working THANK U SNOWPACK CREW

--- public/index.js
+++ public/index.js
@@ -1,9 +1,1 @@
-import { helloWorld } from './helloworld.js';
-import confetti from 'https://cdn.skypack.dev/canvas-confetti';
-
-confetti.create(document.getElementById('canvas'), {
-    resize: true,
-    useWorker: true,
-   })({ particleCount: 200, spread: 200 });
-
-helloWorld();
+function e(){console.log("Hello World!")}import o from"https://cdn.skypack.dev/canvas-confetti";o.create(document.getElementById("canvas"),{resize:!0,useWorker:!0})({particleCount:200,spread:200});e();
